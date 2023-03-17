8f4289d3d3ee3057f8af8ef8b23ec5fd488a3c36
striker <goodluckodemilin@gmail.com>
2023-03-17 09:45:11 +0100

    config monogdb, added cloudinary

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const mongoDb = process.env.MONGODB_URL;
+// Set up mongoose connection
+const dev_db_url = process.env.DEV_DB;
+const mongoDb = process.env.MONGODB_URL || dev_db_url;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
